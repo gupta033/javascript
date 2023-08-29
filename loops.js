b74a6052a4937b249ce948f20fe0576ac9977c93
Ashish Kumar Gupta <117852154+gupta033@users.noreply.github.com>
2023-08-30 00:39:04 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,42 @@
+// for 
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if(element == 5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+    
+// }
+
+
+// table print
+
+for (let i = 1; i <=10; i++) {
+    // console.log(`outer loop: ${i}`);
+    for (let j = 1; j <=10; j++){
+        // console.log(i + '*' + j + '=' + i*j);
+
+}
+    
+}
+
+
+let myarry = ["ashish","kumar","gupta"]
+// console.log(myarry.length);
+for (let i = 0; i < myarry.length; i++) {
+    const element = myarry[i];
+    // console.log(element);
+    
+}
+
+// while and do-whle loop 
+
+// let i=0;
+// while (i<=5) {
+//     console.log(i);
+//     i=i+1;
+    
+// }
+
+console.log("ashish");
