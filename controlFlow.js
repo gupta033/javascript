aa68849cee92fe9e979dcfee08b4435f44414a6e
Ashish Kumar Gupta <117852154+gupta033@users.noreply.github.com>
2023-08-29 23:01:58 +0530

    Add files via upload

---
+++ controlFlow.js
@@ -0,0 +1,62 @@
+if (2=="2") {
+    // console.log("executed");
+}
+  const score = 200
+  if(score>100){
+    let power ="ashish"
+    // console.log(`User power:${score}`);
+  }
+ 
+  const age= 22;
+  if(age<=2){
+    // console.log("comlete btach");
+  }else {
+    // console.log("not comlete btach");
+  }
+
+
+//   const month= 6;
+
+//   switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("Jun");
+//         break;
+  
+//     default :
+//         console.log("Invalid month");
+//         break;
+//   }
+
+const empTyeobj= {}
+
+if(Object.keys(empTyeobj).length ===0){
+    // console.log("object is empty");
+}
+
+// null ?? concept in javascript
+
+let val1;
+// val1 = 5?? 10
+//   val1 = null ?? 10;
+val1= undefined ?? 20;
+
+
+// console.log(val1);
+
+
+// Ternary operater 
+
+let akg =100;
+akg <= 33 ? console.log("less than 100") : console.log("more than 100");
